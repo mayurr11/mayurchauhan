5c4708ef396e4ec6d013cbfd4f742618b6f47874
mayur11 <mayurchauhan1107@gmail.com>
2024-10-01 21:07:16 +0530

    improve Ranking

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.js
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 import { useState, useEffect } from "react";
 import { Footer, Header } from "./components";
 import { Hero, About, Skill, Work, Review, Contact } from "./sections";
@@ -7,36 +7,36 @@ import Preloader from "./components/Preloader"; // Import the Preloader componen
 import "./App.css"; // If you have global styles
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
+	const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000); // Adjust the time for how long you want to show the loader
-    return () => clearTimeout(timer);
-  }, []);
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setLoading(false);
+		}, 2000); // Adjust the time for how long you want to show the loader
+		return () => clearTimeout(timer);
+	}, []);
 
-  return (
-    <>
-      {loading ? (
-        <Preloader /> // Use the Preloader component here
-      ) : (
-        <>
-          <Header />
-          <main>
-            <Hero />
-            <About />
-            <Skill />
-            <Work />
-            <Review />
-            <Contact />
-          </main>
-          <Footer />
-          <Analytics />
-        </>
-      )}
-    </>
-  );
+	return (
+		<>
+			{loading ? (
+				<Preloader /> // Use the Preloader component here
+			) : (
+				<>
+					<Header />
+					<main>
+						<Hero />
+						<About />
+						<Skill />
+						<Work />
+						<Review />
+						<Contact />
+					</main>
+					<Footer />
+					<Analytics />
+				</>
+			)}
+		</>
+	);
 };
 
 export default App;
